dc6311b05783d4ee40ea039db052bbfb7212b098
Safa Doyran <doyransafa@gmail.com>
2023-02-11 21:41:43 +0300

    Post modeli olusturuldu. Post ekleme sayfasi duzenlendi. Anasayfada post listeleme kurgusu yapildi.

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express')
+const mongoose = require("mongoose");
 const ejs = require('ejs')
 const path = require('path')
+const Post = require('./models/Post')
+
 
 const app = express()
+mongoose.set('strictQuery', false);
+mongoose
+    .connect("mongodb://127.0.0.1:27017/cleanblog-test-db")
+    .then(() => console.log("Connected to CleanBlog Posts database!"));
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
-app.get('/', (req,res) => {
-    res.render('index')
-})
-
-app.get('/index', (req,res) => {
-    res.render('index')
+app.get('/', async (req,res) => {
+    const posts = await Post.find({}).sort({postDate: -1}).limit(5)
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    res.render("index", {
+        posts,
+        options
+    });
 })
 
 app.get('/about', (req,res) => {
@@ -25,11 +35,17 @@ app.get('/add_post', (req,res) => {
     res.render('add_post')
 })
 
-app.get('/posts', (req,res) => {
-    res.render('posts')
+app.get('/post', (req,res) => {
+    res.render('post')
 })
 
-const port = 3000
+app.post('/add', async (req,res) => {
+    await Post.create(req.body)
+    await console.log(req.body, 'Added succesfully')
+    await res.redirect('/')
+})
+
+const port = 4000
 app.listen(port, () => {
     console.log(`${port} ile sunucuya baglanildi`)
 })
